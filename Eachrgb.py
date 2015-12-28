d6bbfa814b277a2c545275830ea02ea0da27f667
monthonrose <monthonspy@hotmail.com>
2015-12-28 11:09:30 +0700

    edge sobel success and fill sobel

--- Eachrgb.py
+++ Eachrgb.py
@@ -4,7 +4,9 @@ from skimage import data, io, filters , feature ,morphology
 from scipy import ndimage as ndi
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
-modelcolor = cv2.imread('E:/git/testnc/tum/tum 1641.JPG')
+from skimage import data
+from skimage.filters import threshold_otsu, threshold_adaptive
+modelcolor = cv2.imread('E:/git/testnc/tum/tum 1393.JPG')
 modelcolorgray = cv2.cvtColor(modelcolor,cv2.COLOR_BGR2GRAY)
 imgred = modelcolor[:,:,0]
 cv2.imshow('imgred',imgred)
@@ -18,7 +20,7 @@ edgesgreen = cv2.Canny(imggreen,150,250);
 cv2.imshow('edgesred',edgesred)
 cv2.imshow('edgesblue',edgesblue)
 cv2.imshow('edgesgreen',edgesgreen)
-
+height, width = modelcolor.shape[:2]
 edgesr = filters.sobel(imgred)
 edgesg = filters.sobel(imggreen)
 edgesb = filters.sobel(imgblue)
@@ -60,81 +62,106 @@ cv2.imshow("origin00",edgesrgb)
 cv2.imshow("origin11",edgesr)
 cv2.imshow("origin22",edgesg)
 cv2.imshow("origin33",edgesb)
+copyso = sobelrgb
+copyso = 255-copyso
+cv2.imshow("copysobel",copyso)
 
-#red
-kernel = np.ones((1,2),np.uint8);
-dilationred = cv2.dilate(edgesr,kernel,iterations = 1)
-erosionred = cv2.erode(dilationred,kernel,iterations = 1)
-kernel1 = np.ones((2,1),np.uint8);
-dilationred1 = cv2.dilate(erosionred,kernel1,iterations = 1)
-erosionred1 = cv2.erode(dilationred1,kernel1,iterations = 1)
-height, width = modelcolor.shape[:2]
-crop_imgred = erosionred1[10:height-10, 10:width-10]
-cv2.imshow('dierred',crop_imgred)
-#blue
-dilationblue = cv2.dilate(edgesb,kernel,iterations = 1)
-erosionblue = cv2.erode(dilationblue,kernel,iterations = 1)
-dilationblue1 = cv2.dilate(erosionblue,kernel1,iterations = 1)
-erosionblue1 = cv2.erode(dilationblue1,kernel1,iterations = 1)
-crop_imgblue = erosionblue1[10:height-10, 10:width-10]
-cv2.imshow('dierblue',crop_imgblue)
-#green
-dilationgreen = cv2.dilate(edgesg,kernel,iterations = 1)
-erosiongreen = cv2.erode(dilationgreen,kernel,iterations = 1)
-dilationgreen1 = cv2.dilate(erosiongreen,kernel1,iterations = 1)
-erosiongreen1 = cv2.erode(dilationgreen1,kernel1,iterations = 1)
-crop_imggreen = erosiongreen1[10:height-10, 10:width-10]
-cv2.imshow('diergreen',crop_imggreen)
-#rgb
-dilationgreen = cv2.dilate(edgesrgb,kernel,iterations = 1)
-erosiongreen = cv2.erode(dilationgreen,kernel,iterations = 1)
-dilationgreen1 = cv2.dilate(erosiongreen,kernel1,iterations = 1)
-erosiongreen1 = cv2.erode(dilationgreen1,kernel1,iterations = 1)
-crop_imgrgb = erosiongreen1[10:height-10, 10:width-10]
-cv2.imshow('diergreen',crop_imgrgb)
 
-fill_img = ndi.binary_fill_holes(crop_imgred)
-img_cleaned2 = morphology.remove_small_objects(fill_img, 21)
+block_size = 40
+binary_adaptive = threshold_adaptive(copyso, block_size, offset=10)
+binary_adaptive = np.uint8(binary_adaptive)
+binary_adaptive[binary_adaptive==1] = 255
+binary_adaptive = 255-binary_adaptive
+crop_bisobel = binary_adaptive[10:height-10, 10:width-10]
+cv2.imshow('binary sobel',binary_adaptive)
+fill_img = ndi.binary_fill_holes(crop_bisobel)
+outsobeladaptive = np.uint8(fill_img)
+outsobeladaptive[outsobeladaptive>=1] = 255
+cv2.imwrite('sobelfill.jpg',outsobeladaptive)
 fig, ax = plt.subplots(figsize=(4, 3))
-ax.imshow(img_cleaned2, cmap=plt.cm.gray, interpolation='nearest')
+ax.imshow(fill_img, cmap=plt.cm.gray, interpolation='nearest')
 ax.axis('off')
-ax.set_title('Filling the holes r')
+ax.set_title('adaptiveThreshold')
 
-fill_img = ndi.binary_fill_holes(crop_imggreen)
-img_cleaned3 = morphology.remove_small_objects(fill_img, 21)
-fig, ax = plt.subplots(figsize=(4, 3))
-ax.imshow(img_cleaned3, cmap=plt.cm.gray, interpolation='nearest')
-ax.axis('off')
-ax.set_title('Filling the holes g')
+print binary_adaptive
+binary_adaptive = np.uint8(binary_adaptive)
+binary_adaptive[binary_adaptive==1] = 255
 
-fill_img = ndi.binary_fill_holes(crop_imgblue)
-img_cleaned4 = morphology.remove_small_objects(fill_img, 21)
-fig, ax = plt.subplots(figsize=(4, 3))
-ax.imshow(img_cleaned4, cmap=plt.cm.gray, interpolation='nearest')
-ax.axis('off')
-ax.set_title('Filling the holes b')
-
-fill_img = ndi.binary_fill_holes(crop_imgrgb)
-img_cleaned5 = morphology.remove_small_objects(fill_img, 21)
-fig, ax = plt.subplots(figsize=(4, 3))
-ax.imshow(img_cleaned5, cmap=plt.cm.gray, interpolation='nearest')
-ax.axis('off')
-ax.set_title('Filling the holes rgb')
-
-img_cleaned2 = np.uint8(img_cleaned2)
-img_cleaned3 = np.uint8(img_cleaned3)
-img_cleaned4 = np.uint8(img_cleaned4)
-img_cleaned5 = np.uint8(img_cleaned5)
-
-img_cleaned2[img_cleaned2==1] = 255
-img_cleaned3[img_cleaned3==1] = 255
-img_cleaned4[img_cleaned4==1] = 255
-img_cleaned5[img_cleaned5==1] = 255
-
-io.imsave('outedgesobelrgb.jpg',img_cleaned5)
-io.imsave('outedgesobelr.jpg',img_cleaned2)
-io.imsave('outedgesobelg.jpg',img_cleaned3)
-io.imsave('outedgesobelb.jpg',img_cleaned4)
+cv2.imshow("binary_adaptive",binary_adaptive)
+cv2.imwrite("adaptive.jpg",binary_adaptive)
+# #red
+# kernel = np.ones((1,2),np.uint8);
+# dilationred = cv2.dilate(edgesr,kernel,iterations = 1)
+# erosionred = cv2.erode(dilationred,kernel,iterations = 1)
+# kernel1 = np.ones((2,1),np.uint8);
+# dilationred1 = cv2.dilate(erosionred,kernel1,iterations = 1)
+# erosionred1 = cv2.erode(dilationred1,kernel1,iterations = 1)
+# crop_imgred = erosionred1[10:height-10, 10:width-10]
+# cv2.imshow('dierred',crop_imgred)
+# #blue
+# dilationblue = cv2.dilate(edgesb,kernel,iterations = 1)
+# erosionblue = cv2.erode(dilationblue,kernel,iterations = 1)
+# dilationblue1 = cv2.dilate(erosionblue,kernel1,iterations = 1)
+# erosionblue1 = cv2.erode(dilationblue1,kernel1,iterations = 1)
+# crop_imgblue = erosionblue1[10:height-10, 10:width-10]
+# cv2.imshow('dierblue',crop_imgblue)
+# #green
+# dilationgreen = cv2.dilate(edgesg,kernel,iterations = 1)
+# erosiongreen = cv2.erode(dilationgreen,kernel,iterations = 1)
+# dilationgreen1 = cv2.dilate(erosiongreen,kernel1,iterations = 1)
+# erosiongreen1 = cv2.erode(dilationgreen1,kernel1,iterations = 1)
+# crop_imggreen = erosiongreen1[10:height-10, 10:width-10]
+# cv2.imshow('diergreen',crop_imggreen)
+# #rgb
+# dilationgreen = cv2.dilate(edgesrgb,kernel,iterations = 1)
+# erosiongreen = cv2.erode(dilationgreen,kernel,iterations = 1)
+# dilationgreen1 = cv2.dilate(erosiongreen,kernel1,iterations = 1)
+# erosiongreen1 = cv2.erode(dilationgreen1,kernel1,iterations = 1)
+# crop_imgrgb = erosiongreen1[10:height-10, 10:width-10]
+# cv2.imshow('diergreen',crop_imgrgb)
+#
+# fill_img = ndi.binary_fill_holes(crop_imgred)
+# img_cleaned2 = morphology.remove_small_objects(fill_img, 21)
+# fig, ax = plt.subplots(figsize=(4, 3))
+# ax.imshow(img_cleaned2, cmap=plt.cm.gray, interpolation='nearest')
+# ax.axis('off')
+# ax.set_title('Filling the holes r')
+#
+# fill_img = ndi.binary_fill_holes(crop_imggreen)
+# img_cleaned3 = morphology.remove_small_objects(fill_img, 21)
+# fig, ax = plt.subplots(figsize=(4, 3))
+# ax.imshow(img_cleaned3, cmap=plt.cm.gray, interpolation='nearest')
+# ax.axis('off')
+# ax.set_title('Filling the holes g')
+#
+# fill_img = ndi.binary_fill_holes(crop_imgblue)
+# img_cleaned4 = morphology.remove_small_objects(fill_img, 21)
+# fig, ax = plt.subplots(figsize=(4, 3))
+# ax.imshow(img_cleaned4, cmap=plt.cm.gray, interpolation='nearest')
+# ax.axis('off')
+# ax.set_title('Filling the holes b')
+#
+# fill_img = ndi.binary_fill_holes(crop_imgrgb)
+# img_cleaned5 = morphology.remove_small_objects(fill_img, 21)
+# fig, ax = plt.subplots(figsize=(4, 3))
+# ax.imshow(img_cleaned5, cmap=plt.cm.gray, interpolation='nearest')
+# ax.axis('off')
+# ax.set_title('Filling the holes rgb')
+#
+# img_cleaned2 = np.uint8(img_cleaned2)
+# img_cleaned3 = np.uint8(img_cleaned3)
+# img_cleaned4 = np.uint8(img_cleaned4)
+# img_cleaned5 = np.uint8(img_cleaned5)
+#
+# img_cleaned2[img_cleaned2==1] = 255
+# img_cleaned3[img_cleaned3==1] = 255
+# img_cleaned4[img_cleaned4==1] = 255
+# img_cleaned5[img_cleaned5==1] = 255
+#
+# io.imsave('outedgesobelrgb.jpg',img_cleaned5)
+# io.imsave('outedgesobelr.jpg',img_cleaned2)
+# io.imsave('outedgesobelg.jpg',img_cleaned3)
+# io.imsave('outedgesobelb.jpg',img_cleaned4)
 
 io.show()
 plt.show()
